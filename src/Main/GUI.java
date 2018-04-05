655265f4198f0446414475a451f52d6532a2630b
Radames Juliano Halmeman <rjhalmeman@gmail.com>
2018-04-05 17:41:18 -0300

    Exercício 01 - lista 2

--- src/Main/GUI.java
+++ src/Main/GUI.java
@@ -23,19 +23,40 @@ public class GUI extends JFrame {
 
     private JLabel lbTitulo = new JLabel("Lanches Nerd");
 
-    private JButton btCalcular = new JButton("Calcular");
-
     private JLabel lbQuantidade = new JLabel("Quantidade");
     private JLabel lbLanche = new JLabel("Lanche");
     private JLabel lbPreco = new JLabel("Preço");
     private JLabel lbSubtotal = new JLabel("Subtotal");
 
     private JTextField tfQuantidadeXSalada = new JTextField(5);
-    private JLabel lbXsalada = new JLabel("X-Salada");
-    private JLabel lbPrecoXsalada = new JLabel("12,00");
-    private JLabel lbSubTotalXsalada = new JLabel("0,00");
+    private JLabel lbXSalada = new JLabel("X-Salada");
+    private JLabel lbPrecoXSalada = new JLabel("12,00");
+    private JLabel lbSubTotalXSalada = new JLabel("0,00");
 
-    private JTextField tfResp = new JTextField(5);
+    private JTextField tfQuantidadeXEgg = new JTextField(5);
+    private JLabel lbXEgg = new JLabel("X-Egg");
+    private JLabel lbPrecoXEgg = new JLabel("9,00");
+    private JLabel lbSubTotalXEgg = new JLabel("0,00");
+
+    private JTextField tfQuantidadeXFrango = new JTextField(5);
+    private JLabel lbXFrango = new JLabel("X-Frango");
+    private JLabel lbPrecoXFrango = new JLabel("11,00");
+    private JLabel lbSubTotalXFrango = new JLabel("0,00");
+
+    private JTextField tfQuantidadeXTudo = new JTextField(5);
+    private JLabel lbXTudo = new JLabel("X-Tudo");
+    private JLabel lbPrecoXTudo = new JLabel("17,00");
+    private JLabel lbSubTotalXTudo = new JLabel("0,00");
+
+    private JTextField tfQuantidadeXNerd = new JTextField(5);
+    private JLabel lbXNerd = new JLabel("X-Nerd");
+    private JLabel lbPrecoXNerd = new JLabel("15,33");
+    private JLabel lbSubTotalXNerd = new JLabel("0,00");
+
+    private JButton btCalcular = new JButton("Calcular");
+
+    private JLabel lbTotal = new JLabel("Total");
+    private JLabel lbTotalResp = new JLabel("0,00");
 
     public GUI() {
         setSize(800, 600);
@@ -52,23 +73,75 @@ public class GUI extends JFrame {
         pnNorte.add(lbTitulo);
         pnNorte.setLayout(new FlowLayout(FlowLayout.CENTER));
 
-        pnCentro.setLayout(new GridLayout(1, 4));
+        pnCentro.setLayout(new GridLayout(8, 4));
 
         pnCentro.add(lbQuantidade);
         pnCentro.add(lbLanche);
         pnCentro.add(lbPreco);
         pnCentro.add(lbSubtotal);
 
+        pnCentro.add(tfQuantidadeXEgg);
+        pnCentro.add(lbXEgg);
+        pnCentro.add(lbPrecoXEgg);
+        pnCentro.add(lbSubTotalXEgg);
+
+        pnCentro.add(tfQuantidadeXEgg);
+        pnCentro.add(lbXEgg);
+        pnCentro.add(lbPrecoXEgg);
+        pnCentro.add(lbSubTotalXEgg);
+
+        pnCentro.add(tfQuantidadeXFrango);
+        pnCentro.add(lbXFrango);
+        pnCentro.add(lbPrecoXFrango);
+        pnCentro.add(lbSubTotalXFrango);
+
+        pnCentro.add(tfQuantidadeXTudo);
+        pnCentro.add(lbXTudo);
+        pnCentro.add(lbPrecoXTudo);
+        pnCentro.add(lbSubTotalXTudo);
+
+        pnCentro.add(tfQuantidadeXNerd);
+        pnCentro.add(lbXNerd);
+        pnCentro.add(lbPrecoXNerd);
+        pnCentro.add(lbSubTotalXNerd);
+
+        pnCentro.add(new JLabel(""));
+        pnCentro.add(new JLabel(""));
+        pnCentro.add(new JLabel(""));
+        pnCentro.add(new JLabel(""));
+
+        pnCentro.add(btCalcular);
+        pnCentro.add(new JLabel(""));
+        pnCentro.add(lbTotal);
+        pnCentro.add(lbTotalResp);
+
         btCalcular.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Processamento mp = new Processamento();
-                //enviar dados
-      //          mp.setValor(Double.valueOf(tfA.getText()));
+                Processamento proc = new Processamento();
+                //entrada de dados
+                proc.setQtXSalada(Integer.valueOf(tfQuantidadeXSalada.getText()));
+                proc.setQtXEgg(Integer.valueOf(tfQuantidadeXEgg.getText()));
+                proc.setQtXFrango(Integer.valueOf(tfQuantidadeXFrango.getText()));
+                proc.setQtXTudo(Integer.valueOf(tfQuantidadeXTudo.getText()));
+                proc.setQtXNerd(Integer.valueOf(tfQuantidadeXNerd.getText()));
+                
+                proc.setPrecoXSalada(Double.valueOf(lbPrecoXSalada.getText()));
+                proc.setPrecoXEgg(Double.valueOf(lbPrecoXEgg.getText()));
+                proc.setPrecoXFrango(Double.valueOf(lbPrecoXFrango.getText()));
+                proc.setPrecoXTudo(Double.valueOf(lbPrecoXTudo.getText()));
+                proc.setPrecoXNerd(Double.valueOf(lbPrecoXNerd.getText()));
+                
+                lbSubTotalXSalada.setText(String.valueOf(proc.getSubTotalXSalada()));
+                lbSubTotalXEgg.setText(String.valueOf(proc.getSubTotalXEgg()));
+                lbSubTotalXFrango.setText(String.valueOf(proc.getSubTotalXFrango()));
+                lbSubTotalXTudo.setText(String.valueOf(proc.getSubTotalXTudo()));
+                lbSubTotalXNerd.setText(String.valueOf(proc.getSubTotalXNerd()));
+                
+                lbTotalResp.setText(String.valueOf(proc.getTotal()));
 
                 //obter resposta
-                tfResp.setText(String.valueOf(mp.getResposta()));
-
+                //    tfResp.setText(String.valueOf(mp.getResposta()));
             }
         });
 
