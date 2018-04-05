dab05e2e73615d66bc0f1d28b209947d7f51def6
Radames Juliano Halmeman <rjhalmeman@gmail.com>
2018-04-05 16:52:59 -0300

    Exercício 01 - lista 2

--- src/Main/GUI.java
+++ src/Main/GUI.java
@@ -5,6 +5,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -16,17 +17,23 @@ import javax.swing.JTextField;
 public class GUI extends JFrame {
 
     private Container cp;
-    
-    
+
     JPanel pnNorte = new JPanel();
     JPanel pnCentro = new JPanel();
-    
+
     private JLabel lbTitulo = new JLabel("Lanches Nerd");
-    private JTextField tfA = new JTextField(5);
 
     private JButton btCalcular = new JButton("Calcular");
 
-    private JLabel lbResp = new JLabel("Resp");
+    private JLabel lbQuantidade = new JLabel("Quantidade");
+    private JLabel lbLanche = new JLabel("Lanche");
+    private JLabel lbPreco = new JLabel("Preço");
+    private JLabel lbSubtotal = new JLabel("Subtotal");
+
+    private JTextField tfQuantidadeXSalada = new JTextField(5);
+    private JLabel lbXsalada = new JLabel("X-Salada");
+    private JLabel lbPrecoXsalada = new JLabel("12,00");
+    private JLabel lbSubTotalXsalada = new JLabel("0,00");
 
     private JTextField tfResp = new JTextField(5);
 
@@ -35,26 +42,29 @@ public class GUI extends JFrame {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         cp = getContentPane();
         cp.setLayout(new BorderLayout());
-        
-  //      cp.setLayout(new GridLayout(3, 2));
+
         setTitle("Exercício 1 - Lista 2");
 
         cp.add(pnNorte, BorderLayout.NORTH);
         cp.add(pnCentro, BorderLayout.CENTER);
-        
+
         pnNorte.setBackground(Color.cyan);
         pnNorte.add(lbTitulo);
         pnNorte.setLayout(new FlowLayout(FlowLayout.CENTER));
 
-        pnCen
-        
-        
+        pnCentro.setLayout(new GridLayout(1, 4));
+
+        pnCentro.add(lbQuantidade);
+        pnCentro.add(lbLanche);
+        pnCentro.add(lbPreco);
+        pnCentro.add(lbSubtotal);
+
         btCalcular.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Processamento mp = new Processamento();
                 //enviar dados
-                mp.setValor(Double.valueOf(tfA.getText()));
+      //          mp.setValor(Double.valueOf(tfA.getText()));
 
                 //obter resposta
                 tfResp.setText(String.valueOf(mp.getResposta()));
