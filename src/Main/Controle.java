886390d8bfddda87c3d714618399cff8148071a8
Radames J Halmeman <rjhalmeman@gmail.com>
2018-05-11 10:55:45 -0300

    Ajustes feitos no CRUD zero

--- src/Main/Controle.java
+++ src/Main/Controle.java
@@ -1,4 +1,3 @@
-
 package Main;
 
 import java.util.ArrayList;
@@ -9,37 +8,44 @@ import java.util.List;
  * @author radames
  */
 public class Controle {
-    
+
     List<Contato> lista = new ArrayList<>();
-    
-   
-    
-    public Contato buscar(String chave){                   
-        for (int i = 0; i < lista.size(); i++) {
-            if (chave.equals(lista.get(i).getId())) {
-                return lista.get(i);
-            }           
-        }
-        return null;
-    }
-    
-    public void inserir(Contato contato){
-        lista.add(contato);
-        for (int i = 0; i < lista.size(); i++) {
-            System.out.println(lista.get(i).getId()+"-"+
-                    lista.get(i).getNome());
-        }
-    }
 
     public Controle() {
-         lista.add(new Contato("1","111","11111"));
-        lista.add(new Contato("2","222","22222"));
-        lista.add(new Contato("3","333","33333"));
+        //esse construtor insere dados na lista para facilitar os testes, ao final 
+        //do desenvolvimento deverá ser apagado
+        lista.add(new Contato("1", "111", "11111"));
+        lista.add(new Contato("2", "222", "22222"));
+        lista.add(new Contato("3", "333", "33333"));
+    }
+
+    public Contato buscar(String chave) {
+        for (int i = 0; i < lista.size(); i++) {
+            if (chave.equals(lista.get(i).getId())) {
+                return lista.get(i);//se encontrou, retorna a linha toda (um contato)
+            }
+        }
+        return null; //se não encontrou na lista, retorna um contato nulo
+    }
+
+    public void inserir(Contato contato) {
+        lista.add(contato);
     }
 
     void alterar(Contato contatoOriginal, Contato contatoAlterado) {
-        System.out.println("or "+contatoOriginal.toString());
         lista.set(lista.indexOf(contatoOriginal), contatoAlterado);
     }
-    
+
+    public List<String> listar() {
+        List<String> ls = new ArrayList<>();
+        for (int i = 0; i < lista.size(); i++) {
+            ls.add(""
+                    + lista.get(i).getId() + ";"
+                    + lista.get(i).getNome() + ";"
+                    + lista.get(i).getEndereco()
+            );
+        }
+        return ls;
+    }
+
 }
--- src/Main/GUI.java
+++ src/Main/GUI.java
@@ -1,16 +1,24 @@
 package Main;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.ScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -21,9 +29,12 @@ class GUI extends JFrame {
     private Container cp;
     private JPanel pnNorte = new JPanel(new FlowLayout(FlowLayout.LEFT));
     private JPanel pnCentro = new JPanel(new GridLayout(2, 2));
-    private JPanel pnSul = new JPanel();
+    private CardLayout cardLayout = new CardLayout();
+    private JPanel pnSul = new JPanel(cardLayout);
+    private JPanel pnSulMsg = new JPanel(new GridLayout(1, 1));
+    private JPanel pnSulListagem = new JPanel(new GridLayout(1, 1));
 
-    private JLabel lbChave = new JLabel("Id");
+    private JLabel lbId = new JLabel("Id");
     private JLabel lbNome = new JLabel("Nome");
     private JLabel lbEndereco = new JLabel("Endereço");
 
@@ -32,14 +43,42 @@ class GUI extends JFrame {
     private JTextField tfEndereco = new JTextField(30);
 
     private JButton btBuscar = new JButton("Buscar");
-    private JButton btInserir = new JButton("INS");
-    private JButton btAlterar = new JButton("UPD");
-    private JButton btExcluir = new JButton("DEL");
+    private JButton btInserir = new JButton("Inserir");
+    private JButton btAlterar = new JButton("Alterar");
+    private JButton btExcluir = new JButton("Excluir");
+    private JButton btListar = new JButton("Listar");
     private JButton btSalvar = new JButton("Salvar");
     private JButton btCancelar = new JButton("Cancelar");
-    private boolean inserindo;
-    Controle controle = new Controle();
-    Contato contato = new Contato();
+
+    String[] colunas = new String[]{"Id", "Nome", "Endereço"};
+    String[][] dados = new String[0][3];
+
+    DefaultTableModel model = new DefaultTableModel(dados, colunas);
+    JTable tabela = new JTable(model);
+//                scrollList.add(tabela);
+    JScrollPane scrollList = new JScrollPane();
+
+    private ScrollPane scrollMensagem = new ScrollPane();
+
+    JTextArea textAreaMsg = new JTextArea();
+
+    private boolean inserindo; //esta variável controla se é uma operação de INSERT ou UPDATE no botão salvar
+
+    private Controle controle = new Controle(); //essa é a classe de processamento (controle da lista de contatos)
+    private Contato contato = new Contato(); //ver classe contato
+
+    private void setLog(String msg) {
+
+        textAreaMsg.requestFocus();
+        textAreaMsg.setCaretPosition(textAreaMsg.getDocument().getLength());
+        textAreaMsg.append(msg + "\n");
+    }
+
+    private void travarTextFields(boolean campo) {
+        tfChave.setEditable(campo);
+        tfNome.setEditable(!campo);
+        tfEndereco.setEditable(!campo);
+    }
 
     public GUI() {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -49,7 +88,7 @@ class GUI extends JFrame {
         cp.add(pnCentro, BorderLayout.CENTER);
         cp.add(pnSul, BorderLayout.SOUTH);
 
-        pnNorte.add(lbChave);
+        pnNorte.add(lbId);
         pnNorte.add(tfChave);
         pnNorte.add(btBuscar);
         pnNorte.add(btInserir);
@@ -57,77 +96,70 @@ class GUI extends JFrame {
         pnNorte.add(btExcluir);
         pnNorte.add(btSalvar);
         pnNorte.add(btCancelar);
+        pnNorte.add(btListar);
 
         pnCentro.add(lbNome);
         pnCentro.add(tfNome);
         pnCentro.add(lbEndereco);
         pnCentro.add(tfEndereco);
 
+        scrollMensagem.add(textAreaMsg);
+
+        pnSul.add(pnSulMsg, "pnMsg");
+        pnSul.add(pnSulListagem, "pnLst");
+
+        pnSulMsg.add(scrollMensagem);
+
+        pnSul.setBackground(Color.red);
+
         btSalvar.setVisible(false);
         btCancelar.setVisible(false);
         btInserir.setVisible(false);
         btAlterar.setVisible(false);
         btExcluir.setVisible(false);
+        
+        travarTextFields(true);
 
+// ------------------------BOTAO BUSCAR ----------------------------------------        
         btBuscar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                travarTextFields(true);
+                cardLayout.show(pnSul, "pnMsg");
                 contato = controle.buscar(tfChave.getText());
                 if (contato == null) { //nao achou
                     btInserir.setVisible(true);
                     btAlterar.setVisible(false);
                     btExcluir.setVisible(false);
+                    setLog("Não achou na lista, pode inserir se quiser...");
                 } else { //achou
                     btAlterar.setVisible(true);
                     btExcluir.setVisible(true);
                     tfChave.setText(contato.getId());
                     tfNome.setText(contato.getNome());
                     tfEndereco.setText(contato.getEndereco());
+                    setLog("Achou na lista, pode alterar ou excluir...");
                 }
-
             }
+
         });
 
-        btAlterar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                btSalvar.setVisible(true);
-                btCancelar.setVisible(true);
-                btBuscar.setVisible(false);
-                btAlterar.setVisible(false);
-                btExcluir.setVisible(false);
-                inserindo = false;
-            }
-        });
-
-        btInserir.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                tfNome.requestFocus();
-                btInserir.setVisible(false);
-                btSalvar.setVisible(true);
-                btCancelar.setVisible(true);
-                btBuscar.setVisible(false);
-                inserindo = true;
-            }
-        });
-
+//*********************** BOTÃO SALVAR ****************************************        
         btSalvar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Contato contatoOriginal = contato;
+                Contato contatoOriginal = contato; //para pesquisar na lista
+                //precisamos do contato original (para depois modificar)
 
                 contato.setId(tfChave.getText());
                 contato.setNome(tfNome.getText());
                 contato.setEndereco(tfEndereco.getText());
 
-                if (inserindo) {
-                    contato = new Contato();
-                    contato.setId(tfChave.getText());
+                if (inserindo) { //a variavel inserindo é preenchida nos  
+                    //listenners dos botões alterar ou excluir
+                    contato = new Contato(); //criar um novo contato                   
                     controle.inserir(contato);
-                } else {//alterar
-                    System.out.println("contato original "
-                            + contatoOriginal.toString());
+                } else {//alterar                  
                     controle.alterar(contatoOriginal, contato);
                 }
 
@@ -142,8 +174,58 @@ class GUI extends JFrame {
 
             }
         });
+//############################# BOTAO ALTERAR #################################
+        btAlterar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btSalvar.setVisible(true);
+                btCancelar.setVisible(true);
+                btBuscar.setVisible(false);
+                btAlterar.setVisible(false);
+                btExcluir.setVisible(false);
+                inserindo = false;
+                travarTextFields(false);
+            }
+        });
+//||||||||||||||||||||||||||| BOTÃO INSERIR |||||||||||||||||||||||||||||||||||
+        btInserir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tfNome.requestFocus();
+                btInserir.setVisible(false);
+                btSalvar.setVisible(true);
+                btCancelar.setVisible(true);
+                btBuscar.setVisible(false);
+                inserindo = true;
+                travarTextFields(false);
+            }
+        });
 
-        setSize(700, 150);
+//======================= LISTAR =============================================
+        btListar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(pnSul, "pnLst");
+                scrollList.setPreferredSize(tabela.getPreferredSize());
+                pnSulListagem.add(scrollList);
+                scrollList.setViewportView(tabela);
+                List<String> listaDeContatos = controle.listar();//busca a lista de contatos
+                String[] aux;
+                colunas = new String[]{"Id", "Nome", "Endereço"};
+                dados = new String[0][3];
+                model.setDataVector(dados, colunas);
+                for (int i = 0; i < listaDeContatos.size(); i++) {
+                    aux = listaDeContatos.get(i).split(";");
+                    String[] linha = new String[]{aux[0], aux[1], aux[2]};
+                    // String[] linha = new String[]{"João", "15", "Masculino"};
+                    model.addRow(linha);
+                }
+
+            }
+        });
+
+// parâmetros para janela inicial
+        setSize(700, 200);
         setTitle("Crud contatos");
         setLocationRelativeTo(null);
         setVisible(true);
