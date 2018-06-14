96ff8eb1c20b16bc7455051792dd9b092e1ca163
Radames J Halmeman <rjhalmeman@gmail.com>
2018-06-14 08:27:13 -0300

    Exemplo de uso - gridBagLayout - são 5 exemplos para serem executados individualmente

---
+++ src/ExemplosGridBagLayout/Main.java
@@ -0,0 +1,16 @@
+package ExemplosGridBagLayout;
+
+/**
+ * @author Radames J Halmeman  - rjhalmeman@gmail.com
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+
+        System.out.println("São 5 exemplos, cada um em uma classe \nque deve ser executada individualmente\n"
+                + "clic no nome da classe e escolha executar arquivo");
+
+
+    }
+
+}
